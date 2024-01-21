d2e5bf8fc51187b5ca7d51578c703873bd3d7207
fneri42firenze <filippomartinoneri@gmail.com>
2024-01-21 20:49:35 +0100

    aiut

--- push_swap.c
+++ push_swap.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fneri <fneri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/13 19:00:59 by fneri             #+#    #+#             */
-/*   Updated: 2024/01/21 17:52:41 by fneri            ###   ########.fr       */
+/*   Created: 2024/01/21 16:23:03 by fneri             #+#    #+#             */
+/*   Updated: 2024/01/21 20:30:16 by fneri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int *argc, char **argv)
+void ft_parsing(char argv)
 {
-	t_list	*stack_a;
-	t_list	*stack_b;
-
-	if (argc == 1 || (argc == 2 && !argv[2][0]))
-		return (1);
-	else if (argc == 2)
-		argv = ft_split(argv[1], ' ');
-	if (!ft_parsing(argv))
-		stack_a = ft_create_stack(argv);
-	stack_b = NULL;
-	return (0);
+	if
 }
